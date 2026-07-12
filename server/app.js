7766eecf8c21bd10c1a7d32528e3e216e5f51bcb
Hetvi Tank <hetvitaank07@gmail.com>
2026-07-12 12:04:14 +0530

    Completed authentication module

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
-require("./models/User");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB=require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+require("./models/User");
+
 const app = express();
 connectDB();
 app.use(cors());
@@ -11,8 +14,20 @@ app.use(express.json());
 app.get("/", (req,res)=>{
     res.send("Server Running");
 });
+app.post("/test", (req, res) => {
+    console.log("Test route hit");
+    console.log(req.body);
 
-app.listen(5000,()=>{
-    console.log("Server started");
+    res.json({
+        success: true,
+        message: "Test route working"
+    });
+});
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
 
---
+++ server/controllers/authController.js
@@ -0,0 +1,155 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
+
+// Register User
+const register = async (req, res) => {
+    console.log("1");
+
+    try {
+        console.log("2");
+
+        const { name, email, password } = req.body;
+
+        console.log("3");
+
+        const userExists = await User.findOne({ email });
+
+        console.log("4");
+
+        const salt = await bcrypt.genSalt(10);
+
+        console.log("5");
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        console.log("6");
+
+        const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+        role: "Employee",
+        });
+
+        console.log("7");
+
+        const token = generateToken(user._id);
+
+        console.log("8");
+
+        return res.status(201).json({
+        success: true,
+        token,
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+        success: false,
+        message: error.message,
+        });
+    }
+ };
+/*const register = async (req, res) => {
+    console.log("Register API hit");
+  try {
+    const { name, email, password, department, phone } = req.body;
+
+    // Check if user already exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create User
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      department,
+      phone,
+      role: "Employee", // Fixed as per hackathon requirements
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Registration successful",
+      token: generateToken(user._id),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+*/
+// Login User
+const login = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    // Include password because select:false
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token: generateToken(user._id),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+
+  }
+};
+
+module.exports = {
+  register,
+  login,
+};
